46548af95e4046dfcaddf9cb57c7e0af062fde6c
Fran <francielepa03@gmail.com>
2025-03-24 22:15:49 -0300

    primeira aula

---
+++ app.js
@@ -0,0 +1,16 @@
+/*alert("Olá, seja bem vindo!")
+console.log("Posso escrever aqui também")
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/2)
+alert(10+2)
+console.log(parseFloat("10")+2)
+console.log("10"-2)
+console.log("10"*2)
+console.log("10"/2)
+console.log("10"+2)
+console.log("João" +" " + "Paulo")*/
+
+
+
