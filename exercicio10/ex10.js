ba00215dfeda1a86ddf2ecac11a34fa01286b0e3
Fran <francielepa03@gmail.com>
2025-03-31 21:56:11 -0300

    correção 10

--- exercicio10/ex10.js
+++ exercicio10/ex10.js
@@ -1,8 +1,4 @@
-let cotacaoDolar = parseFloat(prompt("Digite a cotação do dólar:"));
-let valorDolares = parseFloat(prompt("Digite o valor em dólares:"));
-
-
+let cotacaoDolar = prompt("Digite a cotação do dólar:");
+let valorDolares = prompt("Digite o valor em dólares:");
 let valorReais = cotacaoDolar * valorDolares;
-
-
 alert("O valor de " + valorDolares + " dólares equivale a R$ " + valorReais.toFixed(2));
