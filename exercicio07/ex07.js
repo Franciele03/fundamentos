2ebe1deba0bf53af2bf4abbd643e6a54570255d8
Fran <francielepa03@gmail.com>
2025-03-31 21:49:04 -0300

    Exercício 07

---
+++ exercicio07/ex07.js
@@ -0,0 +1,7 @@
+let meses = parseInt(prompt("Digite o número de meses:"));
+
+
+let totalDias = meses * 30;
+
+
+alert("O total de dias para " + meses + " meses, considerando que cada mês tem 30 dias, é: " + totalDias + " dias.");
