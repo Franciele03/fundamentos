ac44338dc7633740d2d8c109233a2632b6b45923
Fran <francielepa03@gmail.com>
2025-03-31 21:49:12 -0300

    Exercício 08

---
+++ exercicio08/ex08.js
@@ -0,0 +1,11 @@
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+let numero3 = parseFloat(prompt("Digite o terceiro número:"));
+let numero4 = parseFloat(prompt("Digite o quarto número:"));
+let numero5 = parseFloat(prompt("Digite o quinto número:"));
+
+
+let media = (numero1 + numero2 + numero3 + numero4 + numero5) / 5;
+
+
+alert("A média dos 5 números é: " + media);
