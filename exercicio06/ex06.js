753769d3c39faf75ab954716df10a090af7662de
Fran <francielepa03@gmail.com>
2025-03-31 21:48:55 -0300

    Exercício 06

---
+++ exercicio06/ex06.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um número qualquer:"));
+
+
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+
+alert("O antecessor do número " + numero + " é " + antecessor + ", e o sucessor é " + sucessor + ".");
