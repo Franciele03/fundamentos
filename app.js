0a1e8a5a29bb5143b9574b2c5cef4c44db54f081
Fran <francielepa03@gmail.com>
2025-04-02 21:15:16 -0300

    NOVAS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let nomeCompleto = ""
-nomeCompleto = prompt("Informe seu nome completo")
-
+function fnSaudacao(){
+    let nomeCompleto = ""
+    nomeCompleto = prompt("Informe seu nome completo ")
+    alert("Seja bem vindo(a) " + nomeCompleto + " ao meu site!")
+    }
 
 function fnTrocarCorDeFundo(){
 let cor = prompt("informe uma cor: \n 1 azul \n2 vermelho \n3 preto \n4 vermelho \n5 rosa \n6 laranja")
@@ -25,8 +27,18 @@ if(cor == "1"){
 document.body.style.backgroundColor = corDeFundo
 }
 
-fnTrocarCorDeFundo()
+function fnMostrarHoraAtual(){
+    let agora = new Date(); // Obtém a data e hora atual
+    let horaFormatada = agora.toLocaleTimeString(); // Formata para exibição
+    alert("Hora atual: " + horaFormatada); // Exibe a hora no alert
+}
 
-
-
+function fnTrocarTitulo(){
+    document.getElementById("titulo").innerText = "XO INSETOS"
+}
+function fdIdiomaPortogues(){
+    document.getElementyById("menu1").innerText = "HOME"
+    document.getElementyById("menu2").innerText = "SERVICES"
+    document.getElementyById("menu3").innerText = "CONTACT"
+}
 
